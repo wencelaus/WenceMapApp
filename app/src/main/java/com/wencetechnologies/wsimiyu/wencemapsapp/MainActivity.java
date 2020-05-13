f63ef260323774b058a074aca2e4b400247a4429
wencelaus <simiyuwencelaus@gmail.com>
2020-05-13 21:21:51 +0300

    Commit

--- app/src/main/java/com/wencetechnologies/wsimiyu/wencemapsapp/MainActivity.java
+++ app/src/main/java/com/wencetechnologies/wsimiyu/wencemapsapp/MainActivity.java
@@ -4,11 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MapView wenceMapView;
+    private ArcGISMap wenceMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wenceMapView = findViewById(R.id.wenceMapView);
+        wenceMap = new ArcGISMap(Basemap.Type.OPEN_STREET_MAP,0.478467 , 37.803972, 7);
+        wenceMapView.setMap(wenceMap);
+
+    }
+    @Override
+    protected void onPause(){
+        wenceMapView.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        wenceMapView.resume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        wenceMapView.dispose();
     }
 }
